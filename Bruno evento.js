66d66fabce1bd5dbdd2f0092e0b22b5573b7582f
Bruno <unosvl@hotmail.com>
2021-09-29 20:35:55 -0300

    alterei o arquivo Bruno evento.js

---
+++ Bruno evento.js
@@ -0,0 +1,18 @@
+var datadoevento = "30";
+
+    if (datadoevento <="30") {
+    console.log ("CADASTRO PERMITIDO "); console.log ("INSERIR SUA IDADE:");
+ }  else console.log ("CADASTRO NEGADO / DATA INVALIDA")
+   
+var idade = "18";
+
+    if (idade >="18") {
+    console.log ("IDADE PERMITIDA"); console.log ("INSIRA SEU NOME:")
+    } else  console.log ("IDADE NÃO PERMITIDA / CADASTRO NEGADO"); {
+
+    } 
+
+var listadeparticipantes = "100";
+   if (listadeparticipantes >"100"){
+       console.log ("CADASTRO NEGADO"); console.log ("LIMITE DE PARTICIPANTES ATINGIDO")
+   } else console.log ("CADASTRO CONCLUÍDO");
