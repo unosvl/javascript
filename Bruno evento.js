23a3b1f7ac2d744de328f5754b3fda8c72476e29
Bruno <unosvl@hotmail.com>
2021-10-02 16:29:35 -0300

    alterei o arquivo Bruno evento.js

--- Bruno evento.js
+++ Bruno evento.js
@@ -1,18 +1,41 @@
-var datadoevento = "30";
-
-    if (datadoevento <="30") {
-    console.log ("CADASTRO PERMITIDO "); console.log ("INSERIR SUA IDADE:");
- }  else console.log ("CADASTRO NEGADO / DATA INVALIDA")
-   
-var idade = "18";
-
-    if (idade >="18") {
-    console.log ("IDADE PERMITIDA"); console.log ("INSIRA SEU NOME:")
-    } else  console.log ("IDADE NÃO PERMITIDA / CADASTRO NEGADO"); {
-
-    } 
-
-var listadeparticipantes = "100";
-   if (listadeparticipantes >"100"){
-       console.log ("CADASTRO NEGADO"); console.log ("LIMITE DE PARTICIPANTES ATINGIDO")
-   } else console.log ("CADASTRO CONCLUÍDO");
+vardataEvento=prompt("agora, Informe a data do evento no formato DD/MM/AAAA");
+varqtdParts=89;
+dataEvento=dataEvento.replaceAll('/','');
+ 
+vardata1=dataEvento.substr(4,4)
+vardata2=dataEvento.substr(2,2)
+vardata3=dataEvento.substr(0,2)
+ 
+//console.log("data 3 2 1 =>  ",data3 + "  " + data2 + "  "+ data1)
+//console.log("dataEvento =>  ",dataEvento)
+ 
+vardata=newDate();
+//console.log("data =>  ",data)
+ 
+varmDia=data.getDate() 
+if (mDia<10) {​​​​​​mDia="0"+mDia }​​​​​​
+//console.log("mDia =>  ",mDia)
+ 
+varmMes= (data.getMonth() +1)
+if (mMes<10) {​​​​​​mMes="0"+mMes }​​​​​​
+//console.log("mMes =>  ",mMes)
+ 
+//const hoje = (data.getDate() + '-' +  mMes + '-' + data.getFullYear()).replaceAll("-","");
+consthoje= (data.getFullYear() +'-'+mMes+'-'+mDia).replaceAll("-","")
+//console.log("hoje =>  ",hoje)
+//if(dataEvento >= hoje){​​​​​​
+// console.log("(data1+ data2 + data3) => " + (data1+ data2 + data3))
+if((data1+data2+data3) >hoje){​​​​​​
+varidade=prompt("Informe a sua idade ?");
+if(idade<18){​​​​​​
+window.alert("Você não tem idade para participar do evento!   Você tem "+idade+" anos.");
+    }​​​​​​elseif(idade>18&&qtdParts<100){​​​​​​
+window.alert(` Concluído com sucesso!   Você tem ${​​​​​​idade}​​​​​​ anos.`)
+    }​​​​​​else {​​​​​​
+window.alert(` A quantidade de participantes já foi atingida.`)
+    }​​​​​​
+}​​​​​​ 
+else{​​​​​​
+window.alert(`Impossível cadastrar um evento para essa data!`)
+//  console.log("Programa concluido com sucesso!")
+}
